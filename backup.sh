7bf3572ff98b3e5db49b196cdb8680f0985ea976
Viacheslav Kaloshin <kiltum@kiltum.tech>
2019-09-24 15:00:33 +0200

    add backup from os x

---
+++ backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Simple backup all private files from user directory
+cd
+now=$(date +"%d_%m_%Y")
+tar cvfz /Users/kiltum/Library/Mobile\ Documents/com\~apple\~CloudDocs/backup_${now}.tar.gz .ssh .gnupg .bash* .config 
