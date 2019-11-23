7da0e97633dd064bc796a26840d693127db2aa80
Viacheslav Kaloshin <kiltum@localhost.localdomain>
2019-11-23 18:34:07 +0300

    fix nvidia waiting for initialisation

--- fedora_nvidia.sh
+++ fedora_nvidia.sh
@@ -2,4 +2,9 @@
 dnf install fedora-workstation-repositories
 dnf config-manager --set-enabled rpmfusion-nonfree-nvidia-driver
 dnf config-manager --set-enabled rpmfusion-nonfree-steam
-dnf install xorg-x11-drv-nvidia akmod-nvidia
+dnf install akmod-nvidia
+echo "blacklist i2c_nvidia_gpu" > /etc/modprobe.d/nvidia.conf
+echo "blacklist nouveau" >> /etc/modprobe.d/nvidia.conf
+dnf remove xorg-x11-drv-nouveau
+mv /boot/initramfs-$(uname -r).img /boot/initramfs-$(uname -r)-nouveau.img
+dracut /boot/initramfs-$(uname -r).img $(uname -r)
