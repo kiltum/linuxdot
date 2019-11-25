147b27966f4ba25abe38def7b7ca973dcde86dbb
Viacheslav Kaloshin <kiltum@kiltum.tech>
2019-11-25 13:05:29 +0300

    fixes after fedora 31

--- fedora.sh
+++ fedora.sh
@@ -1,24 +1,33 @@
+#!/bin/bash
+if ! [ $(id -u) = 0 ]; then
+   echo "This script need to be run under root"
+   sudo $0 $@
+   exit
+fi
 # here is my start up script
-sudo hostnamectl set-hostname piter
-sudo echo "127.0.0.1 piter" >> /etc/hosts
-sudo dnf -y update
-sudo dnf -y install mc bind bind-utils thunderbird
+hostnamectl set-hostname piter
+echo "127.0.0.1 piter" >> /etc/hosts
+dnf -y update
+dnf -y install mc bind bind-utils thunderbird
 # enable my own dns
-sudo systemctl start named
-sudo systemctl enable named
+systemctl start named
+systemctl enable named
 # do not override /etc/resolv.conf by NM
-sudo echo "[main]" >> /etc/NetworkManager/NetworkManager.conf
-sudo echo "dns=none" >> /etc/NetworkManager/NetworkManager.conf
+echo "[main]" >> /etc/NetworkManager/NetworkManager.conf
+echo "dns=none" >> /etc/NetworkManager/NetworkManager.conf
 # override resolv.conf by our values
-sudo echo "search localdomain" > /etc/resolv.conf
-sudo echo "nameserver 127.0.0.1" >> /etc/resolv.conf
+echo "search localdomain" > /etc/resolv.conf
+echo "nameserver 127.0.0.1" >> /etc/resolv.conf
 # lets fill system by useful soft
-sudo dnf -y install snapd
-sudo systemctl start snapd
-sudo snap install bitwarden 
+dnf -y install snapd
+systemctl start snapd
+
 # Sublime Text
-sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-sudo dnf -y install sublime-text
-sudo systemctl enable sshd
-sudo systemctl start sshd
-sudo snap install telegram-desktop
+dnf -y config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+dnf -y install sublime-text
+#enable sshd
+systemctl enable sshd
+systemctl start sshd
+# now snapd part
+snap install telegram-desktop
+snap install bitwarden 
--- fedora_nvidia.sh
+++ fedora_nvidia.sh
@@ -1,10 +1,17 @@
-# its for gaming computer
-dnf install fedora-workstation-repositories
-dnf config-manager --set-enabled rpmfusion-nonfree-nvidia-driver
-dnf config-manager --set-enabled rpmfusion-nonfree-steam
-dnf install akmod-nvidia
+#!/bin/bash
+# this script convert Fedora to "game fedora with nvidia" 
+# and fix "waiting for device initiasation" at boot stage
+if ! [ $(id -u) = 0 ]; then
+   echo "This script need to be run under root"
+   sudo $0 $@
+   exit
+fi
+dnf -y install fedora-workstation-repositories
+dnf -y config-manager --set-enabled rpmfusion-nonfree-nvidia-driver
+dnf -y config-manager --set-enabled rpmfusion-nonfree-steam
+dnf -y install akmod-nvidia steam
 echo "blacklist i2c_nvidia_gpu" > /etc/modprobe.d/nvidia.conf
 echo "blacklist nouveau" >> /etc/modprobe.d/nvidia.conf
-dnf remove xorg-x11-drv-nouveau
+dnf -y remove xorg-x11-drv-nouveau
 mv /boot/initramfs-$(uname -r).img /boot/initramfs-$(uname -r)-nouveau.img
 dracut /boot/initramfs-$(uname -r).img $(uname -r)
